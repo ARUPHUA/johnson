788705483f3f6bd1e12216165b933362d38746b1
ARUPHUA <93768319+ARUPHUA@users.noreply.github.com>
2021-11-11 09:09:51 +0000

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,166 @@
+let isChioma = "Bright";
+const year = 48;
+console.log(isChioma);
+console.log(year)
+let myCar;
+console.log(myCar)
+const maryIsMarried = true;
+let owning =null;
+console.log(owning)
+
+console.log(typeof year)
+const maryWasGoingToSchool = true
+console.log(maryWasGoingToSchool)
+
+
+
+
+
+let firstWord = "Annabel"
+
+
+let thirdWord = "whole"
+let fourthWord = "2"
+let fifthWord ="Chris"
+let sixthWord ="Home"
+
+console.log(`${firstWord} drank the ${thirdWord} ${fourthWord} cups of water before ${fifthWord} could reach ${sixthWord}`)
+
+const myName = "david sampson"
+console.log(myName)
+console.log(myName.toUpperCase())
+
+
+const theWoman ="Charity Mike"
+const myBoy = "charles"
+
+console.log(theWoman.toUpperCase())
+
+console.log(myBoy.toLowerCase())
+
+const ourNames = ["david","peace","emmanuel","cyrus"]
+console.log(ourNames)
+
+const mixed =  ["fit","God","women",39,50,"ruler","ibifiri","emaka","pen","my","sandal"]
+
+console.log(mixed)
+
+console.log(mixed[2])
+console.log(mixed[4])
+console.log(mixed[6])
+console.log(mixed[8])
+console.log(mixed[10])
+
+
+const hood =['mark',88,'gun',90,40,20];
+console.log(hood)
+hood[3] = 23;
+hood[0] = 50;
+hood[2] = "joy";
+
+
+const person = {
+    lastName: "Sampson",
+    firstName: "David",
+    age: 76,
+    language:"English",
+    isMarried: false,
+
+}
+
+console.log(person)
+
+for(let b = 1; b < 358; b++){
+    console.log(b)
+
+}
+
+const newArray = ["judirth","abraham","luvky","abigail","joy","sampsom"]
+for(let x =0; x < 6; x++){
+    console.log(newArray[x])
+}
+
+let answer = 22;
+if(answer == 62){
+    console.log("coke")
+}else{
+    console.log("fanta")
+}
+
+const d =8;
+const y =0;
+const u = 65;
+const s =43;
+const fufu = "kkk"
+const x = "pp"
+ const v = 0;
+ const r = 65;
+ const n = "kkk"
+
+ if(n===fufu){
+     console.log("i understand")
+ }else{
+     console.log("i don't understand")
+ }
+
+ 
+ const m = 7;
+
+ color = m <55? "red" : "blue"
+ console.log(color)
+
+ const age =10;
+ gender =age < 10? "female" :"male"
+ console.log(gender)
+
+ const old =40;
+ color = old > 200? "blue" :"yellow"
+ console.log(color)
+
+
+
+
+ const xy = 70;
+ const ry = 40;
+ const uy = 20;
+ const as =10;
+
+ if(xy===76 || uy===20){
+     console.log("yee")
+ }else{
+     console.log("nee")
+ }
+
+function abigail(){
+    console.log("God is good")
+}
+abigail();
+function toyotsCamry(){
+    console.log("the book id under the table")
+}
+toyotsCamry();
+
+function addingRobot(x,y,z){
+    let r = x + y + z;
+    console.log(r)
+}
+addingRobot(3,5,8);
+addingRobot(4,7,9)
+addingRobot(3,8,1)
+
+const add = ()=>{
+    console.log("this is a boy")
+}
+add();
+
+let anna = 34;
+let briggs = 50;
+let joy = 40;
+
+function addition(anna,briggs,joy){
+    let Q = anna + briggs + joy
+    console.log(Q)
+}
+addition(34,50,40)
+
+console.log(` Anna is ${anna} years old, Briggs is ${briggs} years old, and Joy is ${joy} years old`)
